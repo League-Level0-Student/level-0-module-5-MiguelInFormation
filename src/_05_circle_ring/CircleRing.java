ae05d957212b19e61b1e21a377d81bece1c9b6b3
root <root@091fb0d5e385>
2020-08-27 02:30:10 +0000

    asdsadasda

--- src/_05_circle_ring/CircleRing.java
+++ src/_05_circle_ring/CircleRing.java
@@ -1,4 +1,4 @@
-exxpackage _05_circle_ring;
+package _05_circle_ring;
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -10,16 +10,28 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+Robot r2d2 = new Robot();
 
        //  2. Set your robot’s position to x=150 and y=200
 
+r2d2.moveTo(150, 200);
         // 3. Put the robot's pen down
 
-        // 4. Set the robot’s speed to 10
 
+r2d2.penDown();
+
+        // 4. Set the robot’s speed to 10
+r2d2.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
 
+for (int i = 0; i < 360; i++) {
+	r2d2.move(3);
+	r2d2.turn(1);
+	if(i%20==1) {
+}
+
+	
+}
 	        // 6. Move the robot 3 pixels
 
        //  7. Turn the robot 1 degree
